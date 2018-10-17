445d2562da33fbb1ed1887a9f497d8fea26859e4
Francisco Alanis <francisco.alanis@mongodb.com>
2018-10-17 03:08:18 -0400

    Added bootstrap

--- lang.js
+++ lang.js
@@ -1,7 +1,6 @@
 client = stitch.Stitch.initializeDefaultAppClient('offsite-language-app-oubno');
 db = client.getServiceClient(stitch.RemoteMongoClient.factory, 'mongodb-atlas').db('lang');
 
-
 function displayOnLoad() {
   client.auth
     .loginWithCredential(new stitch.AnonymousCredential())
@@ -9,13 +8,41 @@ function displayOnLoad() {
     .catch(console.error);
 }
 
+function createStringEntry(doc){
+  cls   = "list-group-item list-group-item-action"
+  id    = doc._id
+  //console.log(id)
+  //oid = stitch.ObjectID("5bc6c3081c9d44000097e99a")
+
+  console.log(stitch.ObjectID)
+  //console.log(String(id))
+  text  = doc.content
+  title = text.slice(0, 20)
+
+  listentry  = `<a class="${cls}" id="list-${id}-list" data-toggle="list" href="#list-${id}" role="tab" aria-controls="${id}">${title}</a>`
+  rmbtn      = `<div><button type="button" class="btn btn-default btn-sm" onClick="deleteString('${id}');displayStrings()"><i class="fa fa-trash" aria-hidden="true"></i></button></div>`
+  panelentry = `<div class="tab-pane fade" id="list-${id}" role="tabpanel" aria-labelledby="list-${id}-list">${rmbtn}${text}</div>`
+
+  $("#list-tab").append(listentry)
+  $("#nav-tabContent").append(panelentry)
+}
+
 function displayStrings() {
+  $("#list-tab").empty()
+  $("#nav-tabContent").empty()
   db.collection('strings')
     .find({}, { limit: 100 })
     .asArray()
-    .then(docs => docs.map(doc => `<div>${doc.content}</div>`).join(''))
-    .then(strings => document.getElementById("strings").innerHTML = strings)
+    .then(docs => docs.forEach(doc => createStringEntry(doc)))
     .catch(err => console.log(err));
+}
+
+function deleteString(id){
+  oid = new stitch.ObjectID(id)
+  console.log(id, oid)
+  db.collection("strings")
+    .deleteOne({_id: oid})
+    .catch(err => console.error(err));
 }
 
 function addString() {
