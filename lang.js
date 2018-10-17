b561a721fcd3b32c28b5596e7dd67e40abbfc762
Brian Samek <brian.samek@mongodb.com>
2018-10-17 13:50:56 -0400

    Wrap text in span tags

--- lang.js
+++ lang.js
@@ -13,12 +13,21 @@ function createStringEntry(doc){
   id    = doc._id;
 
   console.log(stitch.ObjectID);
-  text  = doc.content;
+  text = doc.content;
+  textList = text.split(" ");
+  console.log(text);
+  console.log(textList)
+  var words = "";
+  for (i = 0; i < textList.length; i++) {
+    words += "<span>";
+    words += textList[i];
+    words += "</span> ";
+  }
   title = text.slice(0, 20);
 
   listentry  = `<a class="${cls}" id="list-${id}-list" data-toggle="list" href="#list-${id}" role="tab" aria-controls="${id}">${title}</a>`;
   rmbtn      = `<div><button type="button" class="btn btn-default btn-sm" onClick="deleteString('${id}');displayStrings()"><i class="fa fa-trash" aria-hidden="true"></i></button></div>`;
-  panelentry = `<div class="tab-pane fade" id="list-${id}" role="tabpanel" aria-labelledby="list-${id}-list">${rmbtn}${text}</div>`;
+  panelentry = `<div class="tab-pane fade" id="list-${id}" role="tabpanel" aria-labelledby="list-${id}-list">${rmbtn}${words}</div>`;
 
   $("#list-tab").append(listentry);
   $("#nav-tabContent").append(panelentry);
